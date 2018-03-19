2dc717fa444726bfc3ae3db4e2fbf88da5a54edd
Nickolas <35115545+NickolayBauer@users.noreply.github.com>
2018-03-19 23:08:03 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,17 @@
+import telebot
+
+from telebot import types
 token = "555416696:AAFzYRo3GvoPwTNRZI5Rb4fOeC4IdnrnxEc"
 spisok = ["камень", "ножницы", "бумага"]
 tree = 3
 player1 = ''
 player2 = ''
+name1 = ''
+name2 = ''
+id1 = ''
+id2 = ''
+flag = False
+
 
 
 
